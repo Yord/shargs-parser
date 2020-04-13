bb731a50a396eb7c5da39294f04f19fa29472f50
Philipp Wille <philipp.y.wille@gmail.com>
2020-04-13 16:55:16 +0200

    test: test if commandsAsArrays works with defaultValues

--- src/opts/commandsAsArrays.test.js
+++ src/opts/commandsAsArrays.test.js
@@ -15,3 +15,19 @@ test('commandsAsArrays README example works', () => {
 
   expect(opts).toStrictEqual(exp)
 })
+
+test('commandsAsArrays works with defaultValues', () => {
+  const obj = {
+    opts: [
+      {key: 'heroes', types: null, args: ['-h'], array: true, defaultValues: ['Charles', 'Logan']}
+    ]
+  }
+
+  const {opts} = commandsAsArrays(obj)
+
+  const exp = [
+    {key: 'heroes', types: ['string', 'string'], args: ['-h'], array: true, defaultValues: ['Charles', 'Logan']}
+  ]
+
+  expect(opts).toStrictEqual(exp)
+})
