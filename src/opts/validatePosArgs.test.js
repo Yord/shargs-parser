9d9e125e2e7477fafc8deee4929e9c1ce5f2bf05
Philipp Wille <philipp.y.wille@gmail.com>
2020-04-16 11:57:39 +0200

    test: test if validatePosArgs reports invalid required if required is not undefined, true or false

--- src/opts/validatePosArgs.test.js
+++ src/opts/validatePosArgs.test.js
@@ -19,3 +19,20 @@ test('validatePosArgs README example works', () => {
 
   expect(errs).toStrictEqual(exp)
 })
+
+test('validatePosArgs reports invalid required if required is not undefined, true or false', () => {
+  const opts = [
+    {key: 'pos1', types: ['string'], required: true, values: ['Logan']},
+    {key: 'pos2', types: ['string'], required: false, values: ['Charles']},
+    {key: 'pos3', types: ['string'], values: ['Xavier']},
+    {key: 'pos5', types: ['string'], required: 'foo', values: ['Xavier']}
+  ]
+
+  const {errs} = validatePosArgs({opts})
+
+  const exp = [
+    invalidRequiredPositionalArgument({positionalArguments: opts})
+  ]
+
+  expect(errs).toStrictEqual(exp)
+})
