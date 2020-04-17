c50e593a1cab6af9da485a28f3d2d2068bfa2e44
Philipp Wille <philipp.y.wille@gmail.com>
2020-04-17 15:02:31 +0200

    refactor: redefine verifyValuesArity in terms of ducktypes

--- src/opts/verifyValuesArity.js
+++ src/opts/verifyValuesArity.js
@@ -1,6 +1,8 @@
 const traverseOpts = require('./traverseOpts')
 const {invalidArity, invalidTypes, invalidValues} = require('../errors')
 const pipe = require('../combinators/pipe')
+const is = require('../combinators/is')
+const {ArrayVariable, FlagOption, PrimitiveVariable, Unnamed, VariadicVariable} = require('../ducktypes')
 
 module.exports = pipe(
   checkArity('values'),
@@ -14,21 +16,27 @@ function checkArity (key) {
     const {types, [key]: values} = opt
 
     if (Array.isArray(values)) {
-      if (Array.isArray(types)) {
-        if (types.length === 0) {
-          if (values.length !== 1) {
-            errs.push(invalidArity({option: opt}))
-          }
-        } else {
-          if(types.length !== values.length) {
-            errs.push(invalidArity({option: opt}))
-          }
-        }
-      } else if (typeof types === 'undefined') {
+      if (is(PrimitiveVariable)(opt)) {
         if (values.length !== 1) {
           errs.push(invalidArity({option: opt}))
         }
-      } else if (types !== null) {
+      } else if (is(FlagOption)(opt)) {
+        if (values.length !== 1) {
+          errs.push(invalidArity({option: opt}))
+        }
+      } else if (is(ArrayVariable)(opt)) {
+        if (values.length < 2) {
+          errs.push(invalidArity({option: opt}))
+        }
+      } else if (is(Unnamed)(opt)) {
+        if (key === 'defaultValues') {
+          errs.push(invalidValues({[key]: values, option: opt}))
+        } else if (values.length !== 1) {
+          errs.push(invalidArity({types, option: opt}))
+        } else if (typeof values[0] !== 'string') {
+          errs.push(invalidValues({[key]: values, option: opt}))
+        }
+      } else if (!is(VariadicVariable)(opt)) {
         errs.push(invalidTypes({types, option: opt}))
       }
     } else {
