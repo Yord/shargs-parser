8fbaee1ae0252fb36b4f55bd333dd7ee0b1a4f45
Philipp Wille <philipp.y.wille@gmail.com>
2020-04-17 15:04:07 +0200

    test: adjust tests to improved arrayOnRepeat

--- src/opts/arrayOnRepeat.test.js
+++ src/opts/arrayOnRepeat.test.js
@@ -1,4 +1,5 @@
-const {arrayOnRepeat} = require('..')
+const {arrayOnRepeat}     = require('..')
+const {incompatibleTypes} = require('../errors')
 
 test('arrayOnRepeat README example works', () => {
   const obj = {
@@ -11,17 +12,23 @@ test('arrayOnRepeat README example works', () => {
   const {opts} = arrayOnRepeat(obj)
 
   const exp = [
-    {key: 'age', types: ['string', 'number'], values: ['42', 42]},
+    {key: 'age', types: ['string', 'number'], values: ['42', 42]}
   ]
 
   expect(opts).toStrictEqual(exp)
 })
 
-test('arrayOnRepeat works with arrays', () => {
+test('arrayOnRepeat works with arrays, variadic, and singleton, but not commands', () => {
   const obj = {
     opts: [
       {key: 'numBool', types: ['number', 'bool'], values: ['42', 'true']},
-      {key: 'numBool', types: ['number', 'bool'], values: ['23', 'false']}
+      {key: 'numBool', types: ['number', 'bool'], values: ['23', 'false']},
+      {key: 'foo', types: ['string'], values: ['foo']},
+      {key: 'foo', types: ['number'], values: ['42']},
+      {key: 'bar', types: ['string', 'bool'], values: ['foo', 'true']},
+      {key: 'bar', types: ['number'], values: ['42']},
+      {key: 'variadic', values: ['1', '2']},
+      {key: 'variadic', values: ['3']}
     ]
   }
 
@@ -29,6 +36,9 @@ test('arrayOnRepeat works with arrays', () => {
 
   const exp = [
     {key: 'numBool', types: ['number', 'bool', 'number', 'bool'], values: ['42', 'true', '23', 'false']},
+    {key: 'foo', types: ['string', 'number'], values: ['foo', '42']},
+    {key: 'bar', types: ['string', 'bool', 'number'], values: ['foo', 'true', '42']},
+    {key: 'variadic', values: ['1', '2', '3']}
   ]
 
   expect(opts).toStrictEqual(exp)
