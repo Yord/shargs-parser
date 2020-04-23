1369d90cc83b6011016c5e6e8b5bdcd0f7ad682a
Philipp Wille <philipp.y.wille@gmail.com>
2020-04-23 11:49:40 +0200

    test: test if demandACommand records an error if a command has no values or defaultValues

--- src/opts/demandACommand.test.js
+++ src/opts/demandACommand.test.js
@@ -66,6 +66,25 @@ test('demandACommand records no error if two commands are defined', () => {
   expect(errs).toStrictEqual(exp)
 })
 
+test('demandACommand records an error if a command has no values or defaultValues', () => {
+  const opts = [
+    {key: 'title', types: ['string'], values: ["The Hitchhiker's Guide to the Galaxy"]},
+    {key: 'numBool', types: ['number', 'bool'], values: ['23', 'true']},
+    {key: 'answer', types: ['number'], values: ['42']},
+    {key: 'help', args: ['help'], opts: []},
+    {key: 'verbose', types: ['bool'], values: ['false']},
+    {key: 'version', types: [], values: [1]}
+  ]
+
+  const {errs} = demandACommand({opts})
+
+  const exp = [
+    commandRequired({options: opts})
+  ]
+
+  expect(errs).toStrictEqual(exp)
+})
+
 test('demandACommand trows if opts is undefined', () => {
   const obj = {}
 
