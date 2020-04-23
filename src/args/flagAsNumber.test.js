5b7714612a2403b4dc3e12ef049af36a1f945edb
Philipp Wille <philipp.y.wille@gmail.com>
2020-04-23 10:52:56 +0200

    test: test if flagAsNumber works if input is undefined

--- src/args/flagAsNumber.test.js
+++ src/args/flagAsNumber.test.js
@@ -116,3 +116,9 @@ test('flagAsNumber works if opts is undefined', () => {
 
   expect(args).toStrictEqual({_: []})
 })
+
+test('flagAsNumber works if input is undefined', () => {
+  const {args} = flagAsNumber()()
+
+  expect(args).toStrictEqual({_: []})
+})
