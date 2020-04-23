1a4c9e762f943a5832016264d3e2c507e54a0eb4
Philipp Wille <philipp.y.wille@gmail.com>
2020-04-23 10:44:54 +0200

    test: test if flagAsBool does not transform non-flags

--- src/args/flagAsBool.test.js
+++ src/args/flagAsBool.test.js
@@ -1,4 +1,5 @@
 const {flagAsBool} = require('..')
+const pipe = require('../combinators/pipe')
 
 test('flagAsBool README example works', () => {
   const obj = {
@@ -44,6 +45,38 @@ test('flagAsBool works as expected on all types', () => {
     no: undefined,
     nono: null
   }
+
+  expect(args).toStrictEqual(exp)
+})
+
+test('flagAsBool does not transform non-flags', () => {
+  const obj = {
+    args: {
+      title: "The Hitchhiker's Guide to the Galaxy",
+      numBool: [23, true],
+      answer: 42,
+      help: 'foo --bar',
+      verbose: false,
+      version1: {type: 'flag', count: 1},
+      version2: true,
+      no: undefined,
+      nono: null
+    }
+  }
+
+  const {args} = pipe(
+    flagAsBool('title'),
+    flagAsBool('numBool'),
+    flagAsBool('answer'),
+    flagAsBool('help'),
+    flagAsBool('verbose'),
+    flagAsBool('version2'),
+    flagAsBool('no'),
+    flagAsBool('nono'),
+    flagAsBool('foo')
+  )(obj)
+
+  const exp = obj.args
 
   expect(args).toStrictEqual(exp)
 })
