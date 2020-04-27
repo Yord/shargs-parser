3de4b17cb11e35a57ebe53ea6e497cee07f4e884
Philipp Wille <philipp.y.wille@gmail.com>
2020-04-27 10:20:55 +0200

    refactor: make error codes camel case

--- src/errors.js
+++ src/errors.js
@@ -2,7 +2,7 @@ const argumentIsNotABool = (info = {}) => {
   const {values, defaultValues, index, option} = info
   
   return {
-    code: 'Argument is not a boolean',
+    code: 'ArgumentIsNotABoolean',
     msg:  "The passed command line argument must either be 'true' or 'false'",
     info: {values, defaultValues, index, option}
   }
@@ -12,62 +12,62 @@ const argumentIsNotANumber = (info = {}) => {
   const {values, defaultValues, index, option} = info
   
   return {
-    code: 'Argument is not a number',
+    code: 'ArgumentIsNotANumber',
     msg:  'The passed command line argument must be a number',
     info: {values, defaultValues, index, option}
   }
 }
 
 const commandRequired = ({options}) => ({
-  code: 'Command required',
+  code: 'CommandRequired',
   msg:  'No command found. Please use at least one command!',
   info: {options}
 })
 
 const contradictionDetected = ({key, contradicts, option}) => ({
-  code: 'Contradiction detected',
+  code: 'ContradictionDetected',
   msg:  'Some given keys contradict each other.',
   info: {key, contradicts, option}
 })
 
 const didYouMean = ({argv, options}) => ({
-  code: 'Did you mean',
+  code: 'DidYouMean',
   msg:  'An unknown command-line argument was passed. Did you mean any of the following options?',
   info: {argv, options}
 })
 
 const falseArgsRules = ({rules, args}) => ({
-  code: 'False args rules',
+  code: 'FalseArgsRules',
   msg:  'Your args rules returned false. Please abide to the rules defined in verifyArgs.',
   info: {rules, args}
 })
 
 const falseArgvRules = ({rules, argv}) => ({
-  code: 'False argv rules',
+  code: 'FalseArgvRules',
   msg:  'Your argv rules returned false. Please abide to the rules defined in verifyArgv.',
   info: {rules, argv}
 })
 
 const falseOptsRules = ({rules, options}) => ({
-  code: 'False opts rules',
+  code: 'FalseOptsRules',
   msg:  'Your opts rules returned false. Please abide to the rules defined in verifyOpts.',
   info: {rules, options}
 })
 
 const falseRules = ({key, rules, option}) => ({
-  code: 'False rules',
+  code: 'FalseRules',
   msg:  "An option's rules returned false. Please check your arguments.",
   info: {key, rules, option}
 })
 
 const implicationViolated = ({key, implies, option}) => ({
-  code: 'Implication violated',
+  code: 'ImplicationViolated',
   msg:  'Some given keys that imply each other are not all defined.',
   info: {key, implies, option}
 })
 
 const incompatibleTypes = ({opts}) => ({
-  code: 'Incompatible types',
+  code: 'IncompatibleTypes',
   msg:  'Repeated options must either both be variadic or both not.',
   info: {opts}
 })
@@ -76,26 +76,26 @@ const invalidArity = (info = {}) => {
   const {option} = info
   
   return {
-    code: 'Invalid arity',
+    code: 'InvalidArity',
     msg:  "An option's types arity does not match its values arity.",
     info: {option}
   }
 }
 
 const invalidBoolMapping = ({key, alt}) => ({
-  code: 'Invalid bool mapping',
+  code: 'InvalidBoolMapping',
   msg:  "The mapping provided to broadenBools must only map from 'true' or 'false' to a list of alternatives.",
   info: {key, alt}
 })
 
 const invalidRequiredPositionalArgument = ({positionalArguments}) => ({
-  code: 'Invalid required positional argument',
+  code: 'InvalidRequiredPositionalArgument',
   msg:  'If a positional argument is required, all previous positional arguments must be required as well. The required field must either be undefined, true or false.',
   info: {positionalArguments}
 })
 
 const invalidTypes = ({types, option}) => ({
-  code: 'Invalid types',
+  code: 'InvalidTypes',
   msg:  'Each argument must have a types key that must be null or an array',
   info: {types, option}
 })
@@ -104,14 +104,14 @@ const invalidValues = (info = {}) => {
   const {values, defaultValues, option} = info
   
   return {
-    code: 'Invalid values',
+    code: 'InvalidValues',
     msg:  "An option's values field has an invalid type.",
     info: {values, defaultValues, option}
   }
 }
 
 const invalidVariadicPositionalArgument = ({positionalArguments}) => ({
-  code: 'Invalid variadic positional argument',
+  code: 'InvalidVariadicPositionalArgument',
   msg:  'Only the last positional argument may be variadic.',
   info: {positionalArguments}
 })
@@ -120,7 +120,7 @@ const requiredOptionFormat = (info = {}) => {
   const {key, values, defaultValues, option} = info
   
   return {
-    code: 'Wrong format for required option',
+    code: 'WrongFormatForRequiredOption',
     msg:  (
       'A required option has values or defaultValues in the wrong format. ' +
       'Default values are different depending on the command-line option type: ' +
@@ -131,55 +131,55 @@ const requiredOptionFormat = (info = {}) => {
 }
 
 const requiredOptionMissing = ({key, option}) => ({
-  code: 'Required option is missing',
+  code: 'RequiredOptionIsMissing',
   msg:  'An option that is marked as required has not been provided.',
   info: {key, option}
 })
 
 const unexpectedArgument = ({argument}) => ({
-  code: 'Unexpected argument',
+  code: 'UnexpectedArgument',
   msg:  'An unexpected argument was used that has no option defined.',
   info: {argument}
 })
 
 const valueRestrictionsViolated = ({key, values, index, only, option}) => ({
-  code: 'Value restriction violated',
+  code: 'ValueRestrictionViolated',
   msg:  'A value lies outside the allowed values of an option.',
   info: {key, values, index, only, option}
 })
 
 const wrongArgsRulesType = ({type, args}) => ({
-  code: 'Wrong args rules type',
+  code: 'WrongArgsRulesType',
   msg:  'The args rules are of a wrong type, please provide a predicate with the following signature: (args) => boolean',
   info: {type, args}
 })
 
 const wrongArgvRulesType = ({type, argv}) => ({
-  code: 'Wrong argv rules type',
+  code: 'WrongArgvRulesType',
   msg:  'The argv rules are of a wrong type, please provide a predicate with the following signature: (argv) => boolean',
   info: {type, argv}
 })
 
 const wrongContradictsType = ({key, type, option}) => ({
-  code: 'Wrong contradicts type',
+  code: 'WrongContradictsType',
   msg:  'The contradicts field has the wrong type, please provide an array of command-line option keys.',
   info: {key, type, option}
 })
 
 const wrongImpliesType = ({key, type, option}) => ({
-  code: 'Wrong implies type',
+  code: 'WrongImpliesType',
   msg:  'The implies field has the wrong type, please provide an array of command-line option keys.',
   info: {key, type, option}
 })
 
 const wrongOptsRulesType = ({type, options}) => ({
-  code: 'Wrong opts rules type',
+  code: 'WrongOptsRulesType',
   msg:  'The opts rules are of a wrong type, please provide a predicate with the following signature: (options) => boolean',
   info: {type, options}
 })
 
 const wrongRulesType = ({key, type, option}) => ({
-  code: 'Wrong rules type',
+  code: 'WrongRulesType',
   msg:  'The rules have a wrong type, please provide a predicate with the following signature: (option) => (options) => boolean',
   info: {key, type, option}
 })
