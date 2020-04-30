b120d7bff087414e89309a6059c010fcd4cf02a2
Philipp Wille <philipp.y.wille@gmail.com>
2020-04-30 11:51:24 +0200

    test: test if boolAsFlag works as expected on all types

--- src/args/boolAsFlag.test.js
+++ src/args/boolAsFlag.test.js
@@ -17,3 +17,37 @@ test('boolAsFlag README example works', () => {
 
   expect(args).toStrictEqual(exp)
 })
+
+test('boolAsFlag works as expected on all types', () => {
+  const obj = {
+    args: {
+      _: [],
+      title: "The Hitchhiker's Guide to the Galaxy",
+      numBool: [23, true],
+      answer: 42,
+      help: 'foo --bar',
+      verbose: false,
+      version1: {type: 'flag', count: 1},
+      version2: true,
+      no: undefined,
+      nono: null
+    }
+  }
+
+  const {args} = boolAsFlag('verbose')(obj)
+
+  const exp = {
+    _: [],
+    title: "The Hitchhiker's Guide to the Galaxy",
+    numBool: [23, true],
+    answer: 42,
+    help: 'foo --bar',
+    verbose: {type: 'flag', count: -1},
+    version1: {type: 'flag', count: 1},
+    version2: true,
+    no: undefined,
+    nono: null
+  }
+
+  expect(args).toStrictEqual(exp)
+})
