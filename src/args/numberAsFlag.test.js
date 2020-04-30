dcfb4a3ff70aa3767310a38914ed75cb6d02af78
Philipp Wille <philipp.y.wille@gmail.com>
2020-04-30 11:55:37 +0200

    test: test if numberAsFlag works as expected on all types

--- src/args/numberAsFlag.test.js
+++ src/args/numberAsFlag.test.js
@@ -17,3 +17,37 @@ test('numberAsFlag README example works', () => {
 
   expect(args).toStrictEqual(exp)
 })
+
+test('numberAsFlag works as expected on all types', () => {
+  const obj = {
+    args: {
+      _: [],
+      title: "The Hitchhiker's Guide to the Galaxy",
+      numBool: [23, true],
+      answer: 42,
+      help: 'foo --bar',
+      verbose: false,
+      version1: {type: 'flag', count: 1},
+      version2: true,
+      no: undefined,
+      nono: null
+    }
+  }
+
+  const {args} = numberAsFlag('answer')(obj)
+
+  const exp = {
+    _: [],
+    title: "The Hitchhiker's Guide to the Galaxy",
+    numBool: [23, true],
+    answer: {type: 'flag', count: 42},
+    help: 'foo --bar',
+    verbose: false,
+    version1: {type: 'flag', count: 1},
+    version2: true,
+    no: undefined,
+    nono: null
+  }
+
+  expect(args).toStrictEqual(exp)
+})
