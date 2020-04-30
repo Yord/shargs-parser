46226378a7cbd191ee9b6c0b5c3068a8cef76a78
Philipp Wille <philipp.y.wille@gmail.com>
2020-04-30 11:53:33 +0200

    test: test if boolsAsFlags works as expected on all types

--- src/args/boolsAsFlags.test.js
+++ src/args/boolsAsFlags.test.js
@@ -19,3 +19,37 @@ test('boolsAsFlags README example works', () => {
 
   expect(args).toStrictEqual(exp)
 })
+
+test('boolsAsFlags works as expected on all types', () => {
+  const obj = {
+    args: {
+      _: [],
+      title: "The Hitchhiker's Guide to the Galaxy",
+      numBool: [23, true],
+      answer: 42,
+      help: 'foo --bar',
+      verbose: false,
+      version1: {type: 'flag', count: 1},
+      version2: true,
+      no: undefined,
+      nono: null
+    }
+  }
+
+  const {args} = boolsAsFlags(obj)
+
+  const exp = {
+    _: [],
+    title: "The Hitchhiker's Guide to the Galaxy",
+    numBool: [23, true],
+    answer: 42,
+    help: 'foo --bar',
+    verbose: {type: 'flag', count: -1},
+    version1: {type: 'flag', count: 1},
+    version2: {type: 'flag', count: 1},
+    no: undefined,
+    nono: null
+  }
+
+  expect(args).toStrictEqual(exp)
+})
