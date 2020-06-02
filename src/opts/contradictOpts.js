36a912b5e4aaf4d69efd4e43db6327105734af8a
Philipp Wille <philipp.y.wille@gmail.com>
2020-06-02 11:52:06 +0200

    chore: remove defaultValues case from contradictOpts

--- src/opts/contradictOpts.js
+++ src/opts/contradictOpts.js
@@ -1,15 +1,15 @@
 const {traverseOpts} = require('./traverseOpts')
 const {contradictionDetected, wrongContradictsType} = require('../errors')
-const {Contradicts, ValuesOrDefaultValues, Variable} = require('../ducktypes')
+const {Contradicts, Values, Variable} = require('../ducktypes')
 const {is} = require('../combinators/is')
 
-const contradictOpts = traverseOpts(is(Variable, Contradicts, ValuesOrDefaultValues))((opt, _, opts) => {
+const contradictOpts = traverseOpts(is(Variable, Contradicts, Values))((opt, _, opts) => {
   const errs = []
 
   const {key, contradicts: keys} = opt
 
   if (Array.isArray(keys)) {
-    if (opts.some(opt2 => keys.indexOf(opt2.key) > -1 && is(Variable, ValuesOrDefaultValues)(opt2))) {
+    if (opts.some(opt2 => keys.indexOf(opt2.key) > -1 && is(Variable, Values)(opt2))) {
       errs.push(contradictionDetected({key, contradicts: keys, option: opt}))
     }
   } else {
