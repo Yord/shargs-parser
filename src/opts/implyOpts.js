eb7d46e78733a5e1484d5f082a98e70e02ebf111
Philipp Wille <philipp.y.wille@gmail.com>
2020-06-02 11:52:26 +0200

    chore: remove defaultValues case from implyOpts

--- src/opts/implyOpts.js
+++ src/opts/implyOpts.js
@@ -1,15 +1,15 @@
 const {traverseOpts} = require('./traverseOpts')
 const {implicationViolated, wrongImpliesType} = require('../errors')
-const {Implies, ValuesOrDefaultValues, Variable} = require('../ducktypes')
+const {Implies, Values, Variable} = require('../ducktypes')
 const {is} = require('../combinators/is')
 
-const implyOpts = traverseOpts(is(Variable, Implies, ValuesOrDefaultValues))((opt, _, opts) => {
+const implyOpts = traverseOpts(is(Variable, Implies, Values))((opt, _, opts) => {
   const errs = []
 
   const {key, implies: keys} = opt
 
   if (Array.isArray(keys)) {
-    if (!opts.every(opt2 => keys.indexOf(opt2.key) === -1 || is(Variable, ValuesOrDefaultValues)(opt2))) {
+    if (!opts.every(opt2 => keys.indexOf(opt2.key) === -1 || is(Variable, Values)(opt2))) {
       errs.push(implicationViolated({key, implies: keys, option: opt}))
     }
   } else {
